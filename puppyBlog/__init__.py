3d09089bac678e037bb4d7640256354f5d798da6
flawgical <shahzadkhan3iii7@gmail.com>
2022-03-23 13:24:24 -0500

    error pages done - possible started template

--- puppyBlog/__init__.py
+++ puppyBlog/__init__.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 #registering my bluprint to link to my app.py 
 from puppyBlog.core.views import core 
 app.register_blueprint(core)
+
+#linking the 404 and 403 error pages into the app 
+from puppyBlog.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
